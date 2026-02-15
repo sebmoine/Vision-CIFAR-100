0b671f298ec817d8e396e6e024cb8eec869ec2ea
sebmoine <seb.moine@yahoo.fr>
2026-02-15 19:04:33 +0100

    optim updates

--- src/utils/optim.py
+++ src/utils/optim.py
@@ -1,7 +1,6 @@
 import torch
-import torch.nn as nn
 
 def get_optimizer(cfg, params):
-    params_dict = {k: float(v) for k, v in cfg["params"].items()}  # convert float le "1e-3"
+    params_dict = {k: float(v) for k, v in cfg["params"].items() if k=="lr" or k=="weight_decay"}  # convert float le "1e-3"
     optim_class = getattr(torch.optim, cfg["algo"])
     return optim_class(params, **params_dict)
