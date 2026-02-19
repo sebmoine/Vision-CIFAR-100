dbe020cecfe286dc14d4d84796afa14970fb2426
sebmoine <seb.moine@yahoo.fr>
2026-02-19 12:56:41 +0100

    implem of SE Block

---
+++ src/utils/SE.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+# Squeeze-and-Excitation is a building block for CNNs that improves channel interdependencies at almost no computational cost.
+# When first used on ImageNet competition, it improves the result from the previous competition by 25%.
+# The purpose is to add parameters to each channel of a convolutional block so that the network can adaptively adjust the weighting of each feature map
+
+# From "the network weights each of its channels equally when creating the output feature maps" To "content aware mechanism to weight each channel adaptively"
+
+
+# source : Medium - Squeeze-and-Excitation Networks (https://medium.com/data-science/squeeze-and-excitation-networks-9ef5e71eacd7)
+
+class SE_Block(nn.Module):
+    def __init__(self, out_channels, ratio=16): # an input convolutional block and the current number of channels it has
+        super(SE_Block, self).__init__()
+        self.avgpool = nn.GlobalAveragePooling2D()      # 1 x 1 x C
+        self.dense1  = nn.Linear(out_channels//ratio)   # 1 x 1 x C/r
+        self.relu    = nn.ReLU(True)                    # 1 x 1 x C/r
+        self.dense2  = nn.Linear(out_channels)          # 1 x 1 x C
+        self.sigmoid = nn.Sigmoid()                     # 1 x 1 x C
+
+    def forward(self, in_block):
+        x = self.avgpool(in_block)  # squeeze each channel to a single numeric value
+        x = self.dense1(x)          # reduce output channel complexity divided by a ratio
+        x = self.relu(x)            # nonlinearity
+        x = self.dense2(x)
+        x = self.sigmoid(x)         # gives each channel a smooth gating function
+        return torch.mul(x, in_block) # weights each feature map of the convolutional block (in_block) based on the result of our x, a vector of size n=nb of filters in the conv. block.
+        # W x H x C
+
+
+oui = SE_Block(out_channels)(x)
